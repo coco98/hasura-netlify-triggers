f273e1d2fd146dbbc8ea8e8e8df668df84465335
Tanmai Gopal <tanmaig@34cross.in>
2018-11-03 11:11:20 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Netlify functions as Hasura event triggers
+
+This repo has a sample function to get started with using netlify functions as hasura event triggers.
+This trigger doesn't do much except for printing a log.
---
+++ functions/hello.js
@@ -0,0 +1,29 @@
+/* Handle Hasura event trigger
+ * This is what the structure of the event payload looks like
+ *
+ * {
+ *   "event": {
+ *       "op": "<op-name>",
+ *       "data": {
+ *         "old": <old-object>,
+ *         "new": <new-object>
+ *       }
+ *   },
+ *   "created_at": "<timestamp>",
+ *   "id": "<uuid>",
+ *   "trigger": {
+ *       "name": "<name-of-trigger>",
+ *       "id": "<event-uuid>"
+ *   },
+ *   "table":  {
+ *       "schema": "<schema-name>",
+ *       "name": "<table-name>"
+ *   }
+ * }
+ *
+ */
+
+exports.handler = (netlifyEvent, context, cb) => {
+  // Extract JSON payload
+  console.log(event.
+};
