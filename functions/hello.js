6dc79746f6d99f4bba545abb20ba99d9cffee133
Tanmai Gopal <tanmaig@34cross.in>
2018-11-03 13:34:11 -0700

    Sample function

--- functions/hello.js
+++ functions/hello.js
@@ -1,9 +1,37 @@
+/* Handle Hasura event trigger
+ * This is what the structure of the event payload looks like
+ *
+ * {
+ *   "event": {
+ *       "op": "<op-name>",
+ *       "data": {
+ *         "old": <old-object>,
+ *         "new": <new-object>
+ *       }
+ *   },
+ *   "created_at": "<timestamp>",
+ *   "id": "<uuid>",
+ *   "trigger": {
+ *       "name": "<name-of-trigger>",
+ *       "id": "<event-uuid>"
+ *   },
+ *   "table":  {
+ *       "schema": "<schema-name>",
+ *       "name": "<table-name>"
+ *   }
+ * }
+ *
+ */
+
 exports.handler = (lambdaEvent, context, cb) => {
-  const hasuraEventPayload = JSON.parse(lambdaEvent.body);
-  const data = hasuraEventPayload.event.data.new;
-  console.log(data);
+  // Get the email of the user
+  const hasuraTriggerPayload = JSON.parse(lambdaEvent.body);
+
+  console.log(hasuraTriggerPayload);
+  const hasuraData = hasuraTriggerPayload.event.data.new;
+
   cb(null, {
     statusCode: 200,
-    body: JSON.stringify({receivedData: data})
+    body: JSON.stringify({receivedData: hasuraData})
   });
 };
